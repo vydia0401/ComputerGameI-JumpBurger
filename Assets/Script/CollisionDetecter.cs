dc5fa92b61ac1df6f9aa66529c081752624d4a2f
vydia0401 <ylin0@uw.edu>
2022-12-05 16:54:01 +0100

    fixed points

--- Assets/Script/CollisionDetecter.cs
+++ Assets/Script/CollisionDetecter.cs
@@ -14,7 +14,6 @@ public class CollisionDetecter : MonoBehaviour
 
     // Start is called before the first frame update
     void OnCollisionEnter(Collision collision) {
-        Debug.Log(player.name);
         int player1Score = ScoreManager.instance.player1Score;
         int player2Score = ScoreManager.instance.player2Score;
         if (collision.gameObject.name == "baseGround" && player1Score > 0 && player2Score > 0) {
@@ -24,18 +23,18 @@ public class CollisionDetecter : MonoBehaviour
                 ScoreManager.instance.AddPoints(1);
                 collision.gameObject.tag = "player2Unvisted";
             }
-            else if (collision.gameObject.tag == "player1Univisted" ) {
+            else if (collision.gameObject.tag == "player1Unvisted" ) {
                 ScoreManager.instance.AddPoints(1);
-                collision.gameObject.tag = "untagged";
+                collision.gameObject.tag = "Untagged";
             }
         } else if(player.name == "Player2") {
             if (collision.gameObject.tag == "unvisitConcrete") {
                 ScoreManager.instance.AddPoints(2);
                 collision.gameObject.tag = "player1Unvisted";
             }
-            else if (collision.gameObject.tag == "player2Univisted") {
+            else if (collision.gameObject.tag == "player2Unvisted") {
                 ScoreManager.instance.AddPoints(2);
-                collision.gameObject.tag = "untagged";
+                collision.gameObject.tag = "Untagged";
             }
         }
     }
